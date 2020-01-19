dde06b6dd4a31e8b198e4bab53ac3f43bdf613c2
Louis Heery <zcaplhe@ucl.ac.uk>
2020-01-19 19:00:26 +0000

    added einar stuff

---
+++ frontend/src/apiIntegration.js
@@ -0,0 +1,35 @@
+import Cookies from 'js-cookie';
+
+const API_HOST = process.env.REACT_APP_API_HOST;
+
+async function getCsrfToken() {
+    const csrftoken = Cookies.get('csrftoken');
+    if (csrftoken) {
+        return csrftoken;
+    }
+
+    const response = await fetch(`${API_HOST}/csrf/`, {
+        credentials: 'include',
+    });
+    const data = await response.json();
+    return data.csrfToken;
+}
+
+async function fetchArticles() {
+    const response = await fetch(`${API_HOST}/articles`)
+    const articles = await response.json();
+    return articles;
+}
+
+async function postToBackend(endpoint, method, data) {
+    const response = await fetch(`${API_HOST}/${endpoint}/`, {
+        method: method,
+        headers: { 'X-CSRFToken': await getCsrfToken() },
+        credentials: 'include',
+        body: JSON.stringify(data),
+    });
+    const jsonResponse = await response.json();
+    return jsonResponse;
+}
+
+export { getCsrfToken, fetchArticles, postToBackend }
