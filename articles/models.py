c5af5de9c7ce40f26e593f6be9174df6802ff2e6
Max Christl <maxchristl@Maxs-MacBook-Pro.local>
2020-01-24 21:51:40 +0000

    testing if mchristl is now displayed instead of Max Christl

--- articles/models.py
+++ articles/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     publisher = models.ForeignKey('Publisher', on_delete=models.PROTECT, null=True)
     categories = models.ManyToManyField('Category', blank=True)
     text_snippet = models.CharField(max_length=500)
-    text_full = models.TextField(default='') # it makes sense to store the full text (at least in the beginning) since we can only use aylien API 1000 times per day. So if we change our NLP algorithm we can directly test and compare it to the old version without needing to waste our aylien API calls. 
+    text_full = models.TextField(default='') # it makes sense to store the full text (at least in the beginning) since we can only use aylien API 1000 times per day. So if we change our NLP algorithm we can directly test and compare it to the old version without needing to waste our aylien API calls.
     score_model = models.FloatField(null=True)
     score_user_numerator = models.IntegerField(default=0)
     score_user_denominator = models.IntegerField(default=0)
