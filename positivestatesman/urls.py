31f255d9f5b913957ddfb4079958375f884483cf
Einar Jonsson <einarjonsson@mac.com>
2020-01-24 15:21:36 +0000

    added static files urls to urls.py

--- positivestatesman/urls.py
+++ positivestatesman/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/', include('articles.urls')),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('csrf/', views.csrf),
     path('ping/', views.ping),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
