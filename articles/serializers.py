760ad4852000ff639fdbb5f355ea8dd2bfc9d0d8
Einar Jonsson <einarjonsson@mac.com>
2020-01-25 00:54:01 +0000

    serializer now shows related field publisher through publisher field name

--- articles/serializers.py
+++ articles/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from articles.models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
+    publisher = serializers.SlugRelatedField(read_only=True, slug_field='name')
+
     class Meta:
         model = Article
         fields = ['id', 'title', 'url', 'image_url', 'publisher', 'publish_date', 'sentiment_score', 'text_snippet', 'text_full', 'score_user_numerator', 'score_user_denominator', 'categories']
