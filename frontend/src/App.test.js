a65780e9bbb66402c514967d1e9ac4a5c2c74dcc
mchristl <max@christls.de>
2020-02-21 12:16:48 +0000

    yarn test is now working

---
+++ frontend/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
+
+test('App snapshot test', () => {
+  const component = renderer.create(<App />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});j
