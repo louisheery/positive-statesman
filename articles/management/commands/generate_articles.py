9bdcc315a84564ec90a01ee30be09eb1e2bcd5ed
Max Strieth-Kalthoff <m.strieth-kalthoff@gmx.de>
2020-02-22 16:44:26 +0000

    added functionality for backend unittesting

--- articles/management/commands/generate_articles.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from articles.article_fetch import generate_articles
-
-class Command(BaseCommand):
-    help = 'Populates database with new articles'
-
-    def handle(self, *args, **kwargs):
-        generate_articles()
--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,47 @@
+from .models import Article, Publisher, Location, Category, Image
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+
+# Model Tests
+class ModelTest(TestCase):
+
+    def create_Article(self):
+        return Article.objects.create()
+
+    def test_article_functions(self):
+        print("")
+        print("----------------------------------------------------------------------")
+        print("Testing Article Model ...")
+        print("----------------------------------------------------------------------")
+        a = self.create_Article()
+        self.assertTrue(isinstance(a, Article))
+        self.assertEqual(a.user_score(), 0)
+        self.assertEqual(a.__str__(), "")
+        print("OK")
+
+    def create_Publisher(self):
+        return Publisher.objects.create()
+
+    def test_publisher_functions(self):
+        print("")
+        print("----------------------------------------------------------------------")
+        print("Testing Publisher Model ...")
+        print("----------------------------------------------------------------------")
+        a = self.create_Publisher()
+        self.assertTrue(isinstance(a, Publisher))
+        self.assertEqual(a.__str__(), "")
+        print("OK")
+
+    def create_Image(self):
+        return Image.objects.create()
+
+    def test_image_functions(self):
+        print("")
+        print("----------------------------------------------------------------------")
+        print("Testing Image Model ...")
+        print("----------------------------------------------------------------------")
+        a = self.create_Image()
+        self.assertTrue(isinstance(a, Image))
+        self.assertEqual(a.__str__(), "")
+        print("OK")
