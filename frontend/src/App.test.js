c6c019cd791dc93d7191b7f5bcfdca16a7fdc7c3
mchristl <max@christls.de>
2020-02-23 15:14:32 +0000

    added eslint

--- frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
-
-test('App snapshot test', () => {
-  const component = renderer.create(<App />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});j
