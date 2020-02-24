93f54137a16a3fbc7abb50a59b0a2df6bfd0a8de
mchristl <max@christls.de>
2020-02-24 15:01:48 +0000

    added logo tests

--- frontend/src/components/headers/HeaderBar.test.js
+++ frontend/src/components/headers/HeaderBar.test.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import HeaderBar from './HeaderBar'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, MemoryRouter, Route, Redirect } from 'react-router-dom'
 import { FetchMock, fetchMock } from '@react-mock/fetch';
+
+
+describe('user can interact with logo', () => {
+    let getByLabelText
+    beforeEach(() => {
+        ({ getByLabelText } = render(
+            <BrowserRouter>
+                <Redirect to="/business/" />
+                <HeaderBar />
+            </BrowserRouter>
+        ))
+    })
+    test('logo visible', () => {
+        getByLabelText(/logo/i)
+    })
+    test('user get redirected to home page when clicking on logo', () => {
+        fireEvent.click(getByLabelText(/logo/i))
+        expect(location.pathname).toBe('/')
+    })
+
+})
+
 
 
 describe('User can add story successfully', () => {
@@ -46,5 +68,25 @@ describe('User can add story successfully', () => {
         const [, { body }] = fetchMock.lastCall(api, 'POST')
         expect(JSON.parse(body)).toEqual({ url: url })
     })
-
+    test('user can submit the url by hitting enter in text field', () => {
+        fireEvent.click(getByText(/add/i))
+        const textField = getByPlaceholderText(/url/i)
+        fireEvent.change(textField, { target: { value: url } })
+        fireEvent.keyDown(textField, { key: 'Enter', keyCode: 13 })
+        const [, { body }] = fetchMock.lastCall(api, 'POST')
+        expect(JSON.parse(body)).toEqual({ url: url })
+    })
+    test('submit button + text field disappear and add story button reappears after clicking on submit button', () => {
+        fireEvent.click(getByText(/add/i))
+        fireEvent.click(getByText(/submit/i))
+        expect(queryByText(/submit/i)).toBeNull()
+        expect(queryByPlaceholderText(/url/i)).toBeNull()
+        getByText(/add/i)
+    })
+    test('submit button + text field disappear and add story button reappears after hitting enter in text field', () => {
+        fireEvent.click(getByText(/add/i))
+        fireEvent.keyDown(getByPlaceholderText(/url/i), { key: 'Enter', keyCode: 13 })
+        expect(queryByText(/submit/i)).toBeNull()
+    })
 })
+
