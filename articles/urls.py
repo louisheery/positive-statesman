a80b92d6c299f5263d1f4064c8d77d29a12e9de1
leonhard soenke <leonhard.soenke19@imperial.ac.uk>
2020-03-25 09:20:11 +0100

    Added authentication and popular category and publisher fetching paths

--- articles/urls.py
+++ articles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from articles import views
+from django.contrib import admin
 
 
 urlpatterns = [
@@ -8,6 +9,11 @@ urlpatterns = [
     path('fetch-articles/', views.fetch_articles),
     path('user-feedback/', views.user_feedback),
     path('submit-article/', views.submit_article),
+    path('signup/', views.signup),
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('popular/category', views.popular_category),
+    path('popular/publisher', views.popular_publisher),
 ]
 
 '''
